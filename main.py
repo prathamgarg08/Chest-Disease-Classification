aa3da5e80f3f654d488ce6210f1b0f503eb7fcfd
prathamgarg08 <prathamgarg89@gmail.com>
2024-03-29 13:20:32 +0530

    model trainer added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from cnn import logger
 from cnn.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnn.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from cnn.pipeline.stage_03_model_trainer import ModelTrainerTrainingPipeline
 STAGE_NAME= 'Data Ingestion Stage'
 
 try:
@@ -20,6 +20,18 @@ STAGE_NAME='Prepare Base Model Stage'
 try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME='Model Training Stage'
+
+try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
---
+++ src/cnn/pipeline/stage_03_model_trainer.py
@@ -0,0 +1,28 @@
+from cnn.config.configuration import ConfigurationManager
+from cnn.components.model_training import Training
+from cnn import logger
+
+STAGE_NAME="Model Training Stage"
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
